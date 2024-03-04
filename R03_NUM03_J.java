dfd4f6961ed858e727b5aab81cbaee6c002946c8
jo658895 <161987384+jo658895@users.noreply.github.com>
2024-03-03 22:54:45 -0500

    Update R03_NUM03_J.java
    This compliant solution requires that the values read are 32-bit unsigned integers. It reads an unsigned integer value using the readInt() method. The readInt() method assumes signed values and returns a signed int; the return value is converted to a long with sign extension. The code uses an & operation to mask off the upper 32 bits of the long, producing a value in the range of a 32-bit unsigned integer, as intended. The mask size should be chosen to match the size of the unsigned integer values being read.
    As a general principle, you should always be aware of the signedness of the data you are reading.

--- R03_NUM03_J.java
+++ R03_NUM03_J.java
@@ -1,5 +1,5 @@
 // Rule 03 NUM03-J. Use integer types that can fully represent the possible range of unsigned data
 
-public static int getInteger(DataInputStream is) throws IOException {
-    return is.readInt():
+public static long getInteger(DataInputStream is) throws IOException {
+    return is.readInt() & 0xFFFFFFFFL; // Mask with 32 one-bits
 }
