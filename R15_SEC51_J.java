8f01e67e03894adbeea00fc06e0e027c0532cb45
jo658895 <161987384+jo658895@users.noreply.github.com>
2024-03-03 23:08:39 -0500

    Update R15_SEC51_J.java
    Move the call to System.loadLibrary() outside the doPrivileged() block. Doing so allows unprivileged code to perform preliminary password reset checks using the file but prevents it from loading the authentication library.

--- R15_SEC51_J.java
+++ R15_SEC51_J.java
@@ -7,15 +7,17 @@ public void changePassword(String currentPassword, String newPassword) {
     AccessController.doPrivileged(new PrivilegedAction() {
         public Object run() {
             try {
-                String passwordFile = System.getProperty("user.dir") + File.Separator + "PasswordFileName";
+                String passwordFile = (System.getProperty("user.dir") + 
+                                       File.Separator + "PasswordFileName");
                 f[0] = new FileInputStream(passwordFile);
                 // Check whether oldPassword matches the one in the file
                 // If not, throw an exception
-                System.loadLibrary("authentication");
             } catch (FileNotFoundException cnf) {
                 // Forward to handler
             }
             return null;
         }
     }); // End of doPrivileged()
+
+    System.loadLibrary("authentication");
 }
